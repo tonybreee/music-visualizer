e076c9c31016b9170af38820f09c23c2344d0e8b
Tony Hild <tony.hild@mercedes-benz.com>
2025-02-19 03:54:18 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+UPLOAD_FOLDER = "app/uploads"
+OUTPUT_FOLDER = "app/output"
+
+# Spotify API Config
+SPOTIFY_CLIENT_ID = "deine-client-id"
+SPOTIFY_CLIENT_SECRET = "dein-client-secret"
+SPOTIFY_REDIRECT_URI = "http://127.0.0.1:5000/callback/spotify"
